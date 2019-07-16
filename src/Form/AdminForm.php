014d9effd608dc8b6c54ddfa59f0805607487b75
Alan Stanley <astanley@upei.ca>
2019-07-16 09:45:26 -0300

    merged

--- src/Form/AdminForm.php
+++ src/Form/AdminForm.php
@@ -11,59 +11,60 @@ use Drupal\Core\Form\FormStateInterface;
 class AdminForm extends ConfigFormBase {
 
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getFormId() {
-        return 'preservation_reports_admin_form';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'preservation_reports_admin_form';
+  }
 
-    protected function getEditableConfigNames() {
-        return [
-            'preservation_reports.settings',
-        ];
-    }
+  protected function getEditableConfigNames() {
+    return [
+      'preservation_reports.settings',
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        $config = $this->config('preservation_reports.settings');
-        $default_endpoint = $config->get('sparql_endpoint') ? $config->get('sparql_endpoint') : 'http://localhost:8080/bigdata/namespace/islandora/sparql';
-        $form['sparql_endpoint'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('SPARQL endpoint'),
-            '#description' => $this->t('URL to which queries will be sent'),
-            '#maxlength' => 64,
-            '#size' => 64,
-            '#default_value' => $default_endpoint,
-            '#weight' => '0',
-        ];
-        $form['submit'] = [
-            '#type' => 'submit',
-            '#value' => $this->t('Submit'),
-        ];
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('preservation_reports.settings');
+    $default_endpoint = $config->get('sparql_endpoint') ? $config->get('sparql_endpoint') : 'http://localhost:8080/bigdata/namespace/islandora/sparql';
+    $form['sparql_endpoint'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('SPARQL endpoint'),
+      '#description' => $this->t('URL to which queries will be sent.  
+            Address will normally take the form of [PROTOCOL]://[server]:[PORT]/bigdata/namespace/[your namespace (usually Islandora)]/sparql'),
+      '#maxlength' => 64,
+      '#size' => 120,
+      '#default_value' => $default_endpoint,
+      '#weight' => '0',
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
 
-        return $form;
-    }
+    return $form;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
-        parent::validateForm($form, $form_state);
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        // Display result.
-        $values = $form_state->getValues();
-        $this->config('preservation_reports.settings')
-            ->set('sparql_endpoint', $values['sparql_endpoint'])
-            ->save();
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Display result.
+    $values = $form_state->getValues();
+    $this->config('preservation_reports.settings')
+      ->set('sparql_endpoint', $values['sparql_endpoint'])
+      ->save();
 
-    }
+  }
 
 }
--- src/Services/SparqlService.php
+++ src/Services/SparqlService.php
@@ -16,33 +16,30 @@ use Solarium\Core\Client\Adapter\Guzzle;
  * Class SparqlService
  * @package Drupal\islandora_sparql
  */
-
-
-
 class SparqlService {
 
-    private $client;
-    private $config;
+  private $client;
+  private $config;
 
-    public function __construct($client, $config) {
-        $this->client = $client;
-        $this->config = $config;
-    }
+  public function __construct($client, $config) {
+    $this->client = $client;
+    $this->config = $config;
+  }
 
-    public function getQueryResults($query) {
-        $settings = $this->config->get('preservation_reports.settings');
-        $uri = $settings->get('sparql_endpoint');
-        $response = $this->client->request('POST', $uri,
-            [
-                'headers' => [
-                    'Accept' => 'application/sparql-results+json, application/json'
-                ],
-                'form_params' => [
-                    'query' => $query,
-                ],
-            ]);
-        $json = $response->getBody()->getContents();
-        $results = json_decode($json);
-        return $results->results->bindings;
-    }
+  public function getQueryResults($query) {
+    $config = $this->config->get('preservation_reports.settings');
+    $uri = $config->get('sparql_endpoint') ? $config->get('sparql_endpoint') : 'http://localhost:8080/bigdata/namespace/islandora/sparql';
+    $response = $this->client->request('POST', $uri,
+      [
+        'headers' => [
+          'Accept' => 'application/sparql-results+json, application/json',
+        ],
+        'form_params' => [
+          'query' => $query,
+        ],
+      ]);
+    $json = $response->getBody()->getContents();
+    $results = json_decode($json);
+    return $results->results->bindings;
+  }
 }
